6c704c3d4966d96a682bddef95224c55c4777c3a
olgagabrusevich <olgavhab@gmail.com>
2022-05-18 19:46:48 +0300

    hw3 create Hw3Book.js Hw3App.js change App.js

---
+++ skyproject/src/Hw3Book.js
@@ -0,0 +1,16 @@
+function Hw3Book(props) {
+
+  return (
+    <div>
+        <h2>{props.name}</h2>
+        <p>{props.year}</p>
+        <p>{props.price}</p>
+    </div>
+  )   
+}
+export default Hw3Book
+    
+
+
+
+
